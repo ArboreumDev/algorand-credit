c8f73063b353c4a7f6fe2c168be2e44681e77535
djudjuu <julfaber@gmail.com>
2021-09-23 16:14:38 +0200

    basic asset to use for note-field logging

---
+++ scripts/1-assets.js
@@ -0,0 +1,22 @@
+const { globalZeroAddress } = require("@algo-builder/algob");
+
+async function run (runtimeEnv, deployer) {
+  console.log(deployer.accounts)
+  registrar = deployer.accounts[1]
+  const asaInfo = await deployer.deployASA(
+    'supplier1LogAsset',
+    { creator: registrar},
+    {
+      freeze: registrar.addr,
+      manager: registrar.addr,
+  })
+
+  console.log(asaInfo);
+
+  b=await balanceOf(deployer, registrar.addr, asaInfo.assetIndex);
+
+  await deployer.addCheckpointKV('User Checkpoint', 'Deploying Log Asset');
+  // console.log('Sample script for ASC Funding execution has finished!');
+}
+
+module.exports = { default: run };
---
+++ scripts/transfer/log-tx.js
@@ -0,0 +1,36 @@
+/**
+ * Description:
+ * This script does a zero transfer to the creator with some data in the notefield.
+ */
+
+ const { executeTransaction, balanceOf, runtime } = require('@algo-builder/algob');
+ const { types } = require('@algo-builder/web');
+ 
+ async function run (runtimeEnv, deployer) {
+   // query gold ASA from deployer (using checkpoint information),
+   const logASA = deployer.asa.get('supplier1LogAsset')
+   if (logASA === undefined) {
+     console.error('ASA was not deployed. You must run `algob deploy` first.');
+     return;
+   }
+ 
+   // query accounts from config
+   const registrar = deployer.accountsByName.get('registrar');
+ 
+   // execute asset transfer transaction
+   await executeTransaction(deployer, {
+     type: types.TransactionType.TransferAsset,
+     sign: types.SignType.SecretKey,
+     fromAccount: registrar,
+     toAccountAddr: registrar.addr,
+     amount: 0,
+     assetID: logASA.assetIndex,
+     payFlags: { totalFee: 1000, note: "testString" },
+   });
+ 
+   // this will print the balance
+   await balanceOf(deployer, registrar.addr, logASA.assetIndex);
+ }
+ 
+ module.exports = { default: run };
+ 
